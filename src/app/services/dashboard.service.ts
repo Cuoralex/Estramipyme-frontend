e69ec84193a1e39bf4be88499812684b5a5413e6
Sebaspj7 <sebastian.perezj7@gmail.com>
2024-11-23 14:58:47 -0500

    Implementación de lógica para gráficas con backend y bd

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -8,24 +8,27 @@ import { Test } from '../models/test';
   providedIn: 'root',
 })
 export class DashboardService {
-  private baseUrl = 'https://estramipyme-api.vercel.app/usuarios';
-  private baseUrlTest= 'https://estramipyme-api.vercel.app/tests'
+  private baseUrl = 'http://localhost:8080/companies';  // Endpoint del backend para obtener las empresas
+  private baseUrlTest = 'https://estramipyme-api.vercel.app/tests';  // Endpoint del backend para obtener los tests
 
   constructor(private http: HttpClient) {}
 
-  getUsers() {
+  // Obtener todos los usuarios
+  getUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.baseUrl);
   }
 
+  // Obtener usuarios que ya han completado el test
   getUsersWithTestDone(): Observable<User[]> {
     return this.getUsers().pipe(
       map((users: User[]) => users.filter(user => user.isTestDone))
     );
   }
 
+  // Obtener un test por ID
   getTestById(id: string): Observable<Test | undefined> {
     return this.http.get<Test[]>(this.baseUrlTest).pipe(
-      map(tests => tests.find(test => test.id_empresa === id)) // Encuentra el test con el id dado
+      map(tests => tests.find(test => test.id_empresa === id))  // Encuentra el test con el ID dado
     );
   }
 }
--- src/app/views/dashboard-admin/dashboard-admin.component.ts
+++ src/app/views/dashboard-admin/dashboard-admin.component.ts
@@ -1,203 +1,188 @@
 import { Component, OnInit, signal, inject } from '@angular/core';
-import { Chart, ChartType } from 'chart.js/auto'
-import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { Chart, ChartType } from 'chart.js/auto';
 import { DashboardService } from '../../services/dashboard.service';
 import { User } from '../../models/user';
 import { AddStudentButtonComponent } from "../../components/add-student-button/add-student-button.component";
 import { AddTeacherButtonComponent } from "../../components/add-teacher-button/add-teacher-button.component";
+import { NavbarComponent } from "../../components/navbar/navbar.component";
 
 @Component({
   selector: 'app-dashboard-admin',
   standalone: true,
   templateUrl: './dashboard-admin.component.html',
-  styleUrl: './dashboard-admin.component.scss',
-  imports: [NavbarComponent, AddStudentButtonComponent, AddTeacherButtonComponent],
+  styleUrls: ['./dashboard-admin.component.scss'],
+  imports: [AddStudentButtonComponent, AddTeacherButtonComponent, NavbarComponent],
 })
 export class DashboardAdminComponent implements OnInit {
-  lineChart: Chart | undefined;
   pieChart: Chart | undefined;
   radarChart: Chart | undefined;
-  barChart: Chart | undefined;
+  pieChartType: Chart | undefined;
+  barChartTotal: Chart | undefined;
   users = signal<User[]>([]);
   year = signal<string>('2024-');
-  groupedUsersByMonth: any;
   groupedUsersBySize: any;
   groupedUsersBySector: any;
-  groupedUsersByDownload: any;
+  groupedUsersByType: any;
+  totalCompanies: number = 0;
 
   private dashboardService = inject(DashboardService);
 
   ngOnInit(): void {
-    this.getGroupedUsersByMonth();
     this.getGroupedUsersBySize();
     this.getGroupedUsersBySector();
-    this.getGroupedUsersByDownload();
+    this.getGroupedUsersByType();
+    this.getTotalCompanies();
   }
 
   handleYearChange(event: Event) {
     this.year.set((event.target as HTMLInputElement).value);
-    this.getGroupedUsersByMonth();
+    this.getGroupedUsersBySize();
   }
 
-  getGroupedUsersByMonth() {
-    this.dashboardService.getUsers().subscribe((users) => {
-      const usersByYear = users.filter((user) =>
-        user.registerDate?.startsWith(this.year())
-      );
-      const userCountByMonth = Array(12).fill(0);
-      usersByYear.forEach((user) => {
-        const userRegisterDate = user.registerDate != null ? user.registerDate : "2024-01"
-        const month = parseInt(userRegisterDate.split('-')[1], 10) - 1;
-        userCountByMonth[month]++;
-      });
-
-      this.groupedUsersByMonth = userCountByMonth;
-      this.updateLineChart();
-    });
-  }
-
+  // Obtener empresas por tamaño
   getGroupedUsersBySize() {
     this.dashboardService.getUsers().subscribe((users) => {
-      const small = users.filter((user) => user.sizeCompany == '1').length;
-      const medium = users.filter((user) => user.sizeCompany == '2').length;
-      const large = users.filter((user) => user.sizeCompany == '3').length;
+      const small = users.filter((user) => user.sizeCompany === 'Pequeña').length;
+      const medium = users.filter((user) => user.sizeCompany === 'Mediana').length;
+      const large = users.filter((user) => user.sizeCompany === 'Grande').length;
 
       this.groupedUsersBySize = [small, medium, large];
+      this.initializePieChartSize();
     });
-    setTimeout(() => this.initializePieChart(), 1000)
   }
 
+  // Obtener empresas por sector
   getGroupedUsersBySector() {
     this.dashboardService.getUsers().subscribe((users) => {
-      const primary = users.filter((user) => user.sector == '1').length;
-      const secondary = users.filter((user) => user.sector == '2').length;
-      const tertiary = users.filter((user) => user.sector == '3').length;
-      const quaternary = users.filter((user) => user.sector == '4').length;
+      const primary = users.filter((user) => user.sector === 'Agrícola').length;
+      const secondary = users.filter((user) => user.sector === 'Industrial').length;
+      const tertiary = users.filter((user) => user.sector === 'Servicios').length;
+      const quaternary = users.filter((user) => user.sector === 'Construcción').length;
 
       this.groupedUsersBySector = [primary, secondary, tertiary, quaternary];
+      this.initializeRadarChart();
     });
-    setTimeout(() => this.initializeRadarChart(), 1000);
   }
 
-  getGroupedUsersByDownload() {
+  // Obtener empresas por tipo de persona (Natural o Jurídica)
+  getGroupedUsersByType() {
     this.dashboardService.getUsers().subscribe((users) => {
-      const downloaded = users.filter((user) => user.isBookDonwloaded == true).length;
-      const notDownloaded = users.filter((user) => user.isBookDonwloaded == false).length;
+      const natural = users.filter((user) => user.typeUser === 'Natural').length;
+      const juridica = users.filter((user) => user.typeUser === 'Jurídica').length;
 
-      this.groupedUsersByDownload = [downloaded, notDownloaded];
-    });
-    setTimeout(() => this.initiliazeBarChart(), 1000);
-  }
-
-  updateLineChart() {
-    this.lineChart?.destroy();
-
-    this.lineChart = new Chart('lineChart', {
-      type: 'line' as ChartType,
-      data: {
-        labels: [
-          'Enero',
-          'Febrero',
-          'Marzo',
-          'Abril',
-          'Mayo',
-          'Junio',
-          'Julio',
-          'Agosto',
-          'Septiembre',
-          'Octubre',
-          'Noviembre',
-          'Diciembre',
-        ],
-        datasets: [
-          {
-            label: 'Total empresas registradas por mes',
-            data: this.groupedUsersByMonth,
-            fill: false,
-            borderColor: 'rgb(75, 192, 192)',
-            tension: 0.1,
-          },
-        ],
-      },
+      this.groupedUsersByType = [natural, juridica];
+      this.initializePieChartType();
     });
   }
 
-  initializePieChart() {
-    const ctx = document.getElementById('pieChart') as HTMLCanvasElement;
+  // Obtener el total de empresas
+  getTotalCompanies() {
+    this.dashboardService.getUsers().subscribe((users) => {
+      this.totalCompanies = users.length;
+      this.initializeBarChartTotal();
+    });
+  }
+
+  // Inicializar gráfico de Pastel para tamaño de empresas
+  initializePieChartSize() {
+    const ctx = document.getElementById('pieChartSize') as HTMLCanvasElement;
     this.pieChart = new Chart(ctx, {
       type: 'pie' as ChartType,
       data: {
         labels: ['Pequeña', 'Mediana', 'Grande'],
-        datasets: [
-          {
-            label: 'Empresas registradas por tamaño',
-            data: this.groupedUsersBySize,
-            backgroundColor: [
-              'rgb(255, 99, 132,0.9)',
-              'rgb(54, 162, 235,0.9)',
-              'rgb(255, 205, 86,0.9)',
-            ],
-            hoverOffset: 3,
-          },
-        ],
+        datasets: [{
+          label: 'Empresas registradas por tamaño',
+          data: this.groupedUsersBySize,
+          backgroundColor: ['rgb(255, 99, 132, 0.9)', 'rgb(54, 162, 235, 0.9)', 'rgb(255, 205, 86, 0.9)'],
+          hoverOffset: 3,
+        }],
       },
     });
   }
 
+  // Inicializar gráfico de Radar para sector de empresas
   initializeRadarChart() {
     const ctx = document.getElementById('radarChart') as HTMLCanvasElement;
-    console.log(this.groupedUsersBySector)
     this.radarChart = new Chart(ctx, {
       type: 'radar' as ChartType,
       data: {
-        labels: [
-          'Sector Agrícola',
-          'Sector Industrial',
-          'Sector Servicios',
-          'Sector Construcción',
-        ],
-        datasets: [
-          {
-            label: 'Empresas registradas por tamaño',
-            data: this.groupedUsersBySector,
-            backgroundColor: [
-              'rgba(255, 99, 132,0.9)',
-              'rgba(54, 162, 235,0.9)',
-              'rgba(255, 205, 86,0.9)',
-              'rgba(75, 192, 192,0.9)',
-            ],
-            hoverOffset: 4,
-          },
-        ],
+        labels: ['Sector Agrícola', 'Sector Industrial', 'Sector Servicios', 'Sector Construcción'],
+        datasets: [{
+          label: 'Empresas registradas por sector',
+          data: this.groupedUsersBySector,
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+          pointBackgroundColor: 'rgba(54, 162, 235, 1)',
+        }],
       },
       options: {
+        responsive: true,
+        maintainAspectRatio: false,
         scales: {
           r: {
-            angleLines: {
-              display: false,
+            min: 0,
+            max: 20,
+            ticks: {
+              stepSize: 5,
             },
-            suggestedMin: 180,
-            suggestedMax: 270,
+          },
+        },
+        plugins: {
+          legend: {
+            position: 'top',
           },
         },
       },
     });
   }
 
-  initiliazeBarChart() {
-    const ctx = document.getElementById('barChart') as HTMLCanvasElement;
-    this.barChart = new Chart(ctx, {
+  // Inicializar gráfico de Pastel para tipo de persona
+  initializePieChartType() {
+    const ctx = document.getElementById('pieChartType') as HTMLCanvasElement;
+    this.pieChartType = new Chart(ctx, {
+      type: 'pie' as ChartType,
+      data: {
+        labels: ['Natural', 'Jurídica'],
+        datasets: [{
+          label: 'Tipo de usuario registrado',
+          data: this.groupedUsersByType,
+          backgroundColor: ['rgb(153, 102, 255)', 'rgb(255, 159, 64)'],
+          hoverOffset: 3,
+        }],
+      },
+    });
+  }
+  initializeBarChartTotal() {
+    const ctx = document.getElementById('barChartTotal') as HTMLCanvasElement;
+    const maxScale = this.totalCompanies + 100; 
+  
+    this.barChartTotal = new Chart(ctx, {
       type: 'bar' as ChartType,
       data: {
-        labels: ['Lo descargaron', 'No lo descargaron'],
-        datasets: [
-          {
-            label: 'Empresas registradas que descargaron el libro',
-            data: this.groupedUsersByDownload,
-            backgroundColor: ['rgb(255, 99, 132,0.9)', 'rgb(54, 162, 235,0.9)'],
-            barPercentage: 0.5,
+        labels: ['Total de Empresas'],
+        datasets: [{
+          label: 'Número total de usuarios registrados',
+          data: [this.totalCompanies],
+          backgroundColor: 'rgb(75, 192, 192)',
+        }],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          y: {
+            beginAtZero: true,
+            max: 100,     
+            ticks: {
+              stepSize: 20,    
+            },
           },
-        ],
+        },
+        plugins: {
+          legend: {
+            display: false, 
+          },
+        },
       },
     });
   }
